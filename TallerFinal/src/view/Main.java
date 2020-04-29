e0a5f47e78c021e4270c4892dbfa27ea7524132b
daniel m <daniel.martinezvillegas2001@gmail.com>
2020-04-29 10:01:22 -0500

    inicio el programa

---
+++ TallerFinal/src/view/Main.java
@@ -0,0 +1,13 @@
+package view;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PApplet.main("view.Main");
+
+	}
+
+}
