652e3434835e8288027c65565b3866b3c3e268a5
AlejandroVal99 <joalevalverde@hotmail.com>
2020-05-04 23:45:15 -0500

    TXT y su lectura, con algunas variables

--- TallerFinal/src/model/Logic.java
+++ TallerFinal/src/model/Logic.java
@@ -4,45 +4,64 @@ import java.util.*;
 
 import processing.core.PApplet;
 
-
 public class Logic {
-	private PApplet app; 
-	private User usuario; 
+	private PApplet app;
+	private User usuario;
+	private String infoPokemons[];
 	private LinkedList<User> usuariosList;
+	private LinkedList<Pokemon> pokemonList;
 	private UserNicknameCompare userNick;
-	
-	
-	
-
+	private PokemonTipoCompare pokemonTipo;
 
 	public Logic(PApplet app) {
-		this.app= app; 
+		this.app = app;
+		infoPokemons = app.loadStrings("../data/Informacion/pokemones.txt");
 		usuariosList = new LinkedList<User>();
-		
-		
-		
-		
-		
-		
+		pokemonList = new LinkedList<Pokemon>();
+		userNick = new UserNicknameCompare();
+		pokemonTipo = new PokemonTipoCompare();
+
 	}
-	
+
+	public void crearPokemones() {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < infoPokemons.length; i++) {
+
+			String[] datosPokemons = infoPokemons[i].split(",");
+
+			String nombre = datosPokemons[0];
+			String tipo = datosPokemons[1];
+			int nivel = Integer.parseInt(datosPokemons[2]);
+			int dano1 = Integer.parseInt(datosPokemons[3]);
+			int dano2 = Integer.parseInt(datosPokemons[4]);
+			int xP = Integer.parseInt(datosPokemons[5]);
+			int posx = Integer.parseInt(datosPokemons[6]);
+			int posy = Integer.parseInt(datosPokemons[7]);
+
+
+			if (infoPokemons[1].equals("Agua")) {
+				pokemonList.add(new Agua(nombre, tipo, this.app, dano1, dano2, xP, nivel, posx, posy));
+			}else if (infoPokemons[1].equals("Fuego")) {
+				pokemonList.add(new Fuego(nombre, tipo, this.app, dano1, dano2, xP, nivel, posx, posy));
+			} else {
+				pokemonList.add(new Hierva(nombre, tipo, this.app, dano1, dano2, xP, nivel, posx, posy));
+			}
+
+		}
+	}
+
 	public void creo() {
-		usuario = new User(app, "daniel", "hola123"); 
+		usuario = new User(app, "daniel", "hola123");
 	}
-	
-	
+
 	public void test1() {
 		usuario.pintar();
-		
+
 	}
-	
 
 	public void moverPersonaje(char k) {
 		usuario.moverse(k);
-		
-		
+
 	}
-	
-	
 
 }
